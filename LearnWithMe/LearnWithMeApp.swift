65808bcdcafeb7e6fa99aaa75a2bf63779800a18
jpotechin <45020840+jpotechin@users.noreply.github.com>
2020-10-21 15:48:04 -0700

    Initial Commit

---
+++ LearnWithMe/LearnWithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnWithMeApp.swift
+//  LearnWithMe
+//
+//  Created by Joseph Potechin on 10/21/20.
+//
+
+import SwiftUI
+
+@main
+struct LearnWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
