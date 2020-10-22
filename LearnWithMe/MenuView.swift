b06c5b379943a27017ef93fac2d69a328f5c2016
jpotechin <45020840+jpotechin@users.noreply.github.com>
2020-10-22 16:18:21 -0700

    Created Home View and Menu View with transition between home and menu

---
+++ LearnWithMe/MenuView.swift
@@ -0,0 +1,73 @@
+//
+//  MenuView.swift
+//  LearnWithMe
+//
+//  Created by Joseph Potechin on 10/22/20.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            VStack(spacing: 16) {
+                Text("Meng - 28% Complete")
+                    .font(.caption)
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.08))
+                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.1))
+                
+                MenuRow(title: "Account", icon: "gear")
+                MenuRow(title: "Billing", icon: "creditcard")
+                MenuRow(title: "Sign out", icon: "person.crop.circle")
+                
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 300)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.8705882353, green: 0.8941176471, blue: 0.9450980392, alpha: 1))]), startPoint: .top, endPoint: .bottom))
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.2), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
+            .padding(.horizontal, 30)
+            .overlay(
+                Image("Avatar")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    .frame(width: 60, height: 60)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .offset(y: -150)
+            )
+        }
+        .padding(.bottom, 30)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
+
+struct MenuRow: View {
+    var title: String;
+    var icon: String;
+    
+    var body: some View {
+        HStack(spacing: 16) {
+            Image(systemName: icon)
+                .font(.system(size: 20, weight: .light, design: .default))
+                .imageScale(.large)
+                .frame(width: 32, height: 32)
+                .foregroundColor(Color(#colorLiteral(red: 0.662745098, green: 0.7333333333, blue: 0.831372549, alpha: 1)))
+            Text(title)
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .frame(width: 120, alignment: .leading)
+        }
+    }
+}
