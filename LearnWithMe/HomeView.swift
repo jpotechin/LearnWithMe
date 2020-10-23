2627cb13468effeb81c6d5cd7d2aea8ff0243cbd
jpotechin <45020840+jpotechin@users.noreply.github.com>
2020-10-22 20:48:18 -0700

    Created Dummy Data and iterated over data

--- LearnWithMe/HomeView.swift
+++ LearnWithMe/HomeView.swift
@@ -21,17 +21,17 @@ struct HomeView: View {
             .padding(.horizontal)
             .padding(.leading, 14)
             .padding(.top, 30)
-            
+
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 30) {
-                    ForEach(0 ..< 5) { item in
-                        SectionView()
+                    ForEach(sectionData) { item in
+                        SectionView(section: item)
                     }
                 }
                 .padding(30)
                 .padding(.bottom, 30)
             }
-            
+
             Spacer()
         }
     }
@@ -44,19 +44,20 @@ struct HomeView_Previews: PreviewProvider {
 }
 
 struct SectionView: View {
+    var section: Section
     var body: some View {
         VStack {
             HStack(alignment: .top) {
-                Text("Prototype designs in SwiftUi")
+                Text(section.title)
                     .font(.system(size: 24, weight: .bold))
                     .frame(width: 160, alignment: .leading)
                     .foregroundColor(.white)
                 Spacer()
-                Image("Logo1")
+                Image(section.logo)
             }
-            Text("18 Sections".uppercased())
+            Text(section.text.uppercased())
                 .frame(maxWidth: .infinity, alignment: .leading)
-            Image("Card1")
+            section.image
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 210)
@@ -64,8 +65,23 @@ struct SectionView: View {
         .padding(.top, 20)
         .padding(.horizontal, 20)
         .frame(width: 275, height: 275)
-        .background(Color("card1"))
+        .background(section.color)
         .cornerRadius(30)
-        .shadow(color: Color("card1").opacity(0.3), radius: 20, x: 0, y: 20)
+        .shadow(color: section.color.opacity(0.3), radius: 20, x: 0, y: 20)
     }
 }
+
+struct Section: Identifiable {
+    var id = UUID()
+    var title: String
+    var text: String
+    var logo: String
+    var image: Image
+    var color: Color
+}
+
+let sectionData = [
+    Section(title: "Prototype designs in SwiftUI", text: "18 Sections", logo: "Logo1", image: Image("Card1"), color: Color("card1")),
+    Section(title: "Build A SwiftUI App", text: "20 Sections", logo: "Logo1", image: Image("Card2"), color: Color("card2")),
+    Section(title: "SwiftUI Advanced", text: "30 Sections", logo: "Logo1", image: Image("Card3"), color: Color("card3")),
+]
